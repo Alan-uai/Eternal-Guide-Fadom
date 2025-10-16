b5ccf6df965615abe389bebd855b8331415ae995
Alan Santos Coutinho <alanscoutinho0@gmail.com>
2025-10-16 17:12:16 +0000

    Mais informações para a Wiki

--- src/lib/wiki-data.ts
+++ src/lib/wiki-data.ts
@@ -19,6 +19,7 @@ import { standsArticle } from './wiki-articles/stands';
 import { starChancesArticle } from './wiki-articles/star-chances';
 import { swordsArticle } from './wiki-articles/swords';
 import { titansArticle } from './wiki-articles/titans';
+import { upgradesCostsArticle } from './wiki-articles/upgrades-costs';
 import { world20RaidsArticle } from './wiki-articles/world-20-raids';
 import { worldBossesArticle } from './wiki-articles/world-bosses';
 
@@ -45,6 +46,7 @@ export const allWikiArticles = [
     lobbyDungeonsArticle,
     achievementsGuideArticle,
     starChancesArticle,
+    upgradesCostsArticle,
 ];
 
 // Adiciona o timestamp do servidor para cada artigo
